f5706cc2938642e9b911035758e7c6852b441138
Chester-OZ <153185988+Chester-OZ@users.noreply.github.com>
2024-03-12 18:09:47 +0200

    hw1-v02

--- js/task-3.js
+++ js/task-3.js
@@ -2,7 +2,7 @@ function getElementWidth(content, padding, border) {
   const getContent = Number.parseFloat(content);
   const getPadding = Number.parseFloat(padding);
   const getBorder = Number.parseFloat(border);
-  return (getContent + (getPadding + getBorder) * 2).toFixed(1);
+  return getContent + getPadding * 2 + getBorder * 2;
 }
 
 console.log(getElementWidth("50px", "8px", "4px"));
